41298dc10936ce4c36e552bc26a21a6012d2edb7
ginte.karbauskaite1@gmail.com <ginte.karbauskaite1@gmail.com>
2021-12-11 20:17:33 +0200

    Refactored code

--- AdventOfCode3/Program.cs
+++ AdventOfCode3/Program.cs
@@ -9,78 +9,127 @@ namespace AdventOfCode3
         static void Main(string[] args)
         {
             var data = File.ReadAllLines("Prefab.txt");
+            var lineLength = data.First().Length;
 
-            var countOfOnes = new int[5];
-            var countOfZeros = new int[5];
-            //var gammaRate = new int[5];
-            //var epsilonRate = new int[5];
+            var countOfOnes = GetCountOfOnes(data, lineLength);
+            var countOfZeros = GetCountOfZeros(data, lineLength);
+
+            var gammaRate = GetGammaRate(countOfOnes, countOfZeros, lineLength);
+            var epsilonRate = GetEpsilonRate(countOfOnes, countOfZeros, lineLength);
+
+            var oxyRating = GetOxyRate(data, lineLength);
+            var co2Rating = GetCo2Rate(data, lineLength);
+
+
+            Console.WriteLine($"Count of Ones: {string.Join(",", countOfOnes)}");
+            Console.WriteLine($"Count of Zeros: {string.Join(",", countOfZeros)}\n");
+            Console.WriteLine($"Gamma Rate: {string.Join(",", gammaRate)}");
+            Console.WriteLine($"Epsilon Rate: {string.Join(",", epsilonRate)}\n");
+            //Console.WriteLine($"Oxygen rate: {string.Join(",", oxyRating)}");
+            //Console.WriteLine($"CO2 rate: {string.Join(",", co2Rating)}");
+        }
+
+        public static int[] GetCountOfOnes(string[] data, int length)
+        {
+            var Ones = new int[length];
 
             foreach (var character in data)
             {
-                Console.WriteLine($"Bits: {character}; Length: {character.Length};");
+                //Console.WriteLine($"Bits: {character}; Length: {character.Length};");
+
+                for (int i = 0; i < character.Length; i++)
+                {
+                    if (character[i] == '1')
+                        Ones[i]++;
+                }
+            }
+
+            return Ones;
+        }
+
+        public static int[] GetCountOfZeros(string[] data, int length)
+        {
+            var countOfZeros = new int[length];
+
+            foreach (var character in data)
+            {
+                //Console.WriteLine($"Bits: {character}; Length: {character.Length};");
 
                 for (int i = 0; i < character.Length; i++)
                 {
                     //Console.WriteLine($"Char: {character[i]};");
-                    if (character[i] == '1')
-                        countOfOnes[i]++;
-
                     if (character[i] == '0')
                         countOfZeros[i]++;
                 }
-
-                //Console.WriteLine($"Count of Ones: {countOfOnes[0]}");
                 //Console.WriteLine($"Count of Zeros: {countOfZeros[0]}");
-
-
-                //foreach (var c in character)
-                //{
-                //    Console.WriteLine($"Char: {c}");
-                //    if (c == '1')
-                //        countOfOnes[i]++;
-
-                //    if (c == '0')
-                //        countOfZeros[i]++;
-                //}
-
-                //for (int i = 0; i < character.Length; i++)
-                //{
-                //    Console.WriteLine(character[i]);
-                //    //if (character[i] == '1')
-                //    //    countOfOnes[character.Length - (character.Length - i)]++;
-
-                //    //countOfZeros[character.Length - (character.Length - i)]++;
-                //}
             }
 
-            Console.WriteLine($"Count of Ones: {string.Join(",", countOfOnes)}");
-            Console.WriteLine($"Count of Zeros: {string.Join(",", countOfZeros)}");
-
-            //foreach (var character in data)
-            //{
-            //    for (int i = 0; i < character.Length; i++)
-            //    {
-            //        character
-            //            .Where(s => countOfOnes[i] > countOfZeros[i]);
-            //    }
-            //}
-
-            //for (int i = 0; i < 12; i++)
-            //{
-            //    if (countOfOnes[i] > countOfZeros[i])
-            //    {
-            //        gammaRate[i] = 1;
-            //        epsilonRate[i] = 0;
-            //    }
-            //    else
-            //    {
-            //        gammaRate[i] = 0;
-            //        epsilonRate[i] = 1;
-            //    }
-            //}
-
-            //Console.WriteLine($"Gamma Rate: {string.Join(",", gammaRate)}");
-            //Console.WriteLine($"Epsilon Rate: {string.Join(",", epsilonRate)}");
+            return countOfZeros;
         }
+
+        public static int[] GetGammaRate(int[] countOfOnes, int[] countOfZeros, int length)
+        {
+            var gammaRate = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                if (countOfOnes[i] > countOfZeros[i])
+                {
+                    gammaRate[i] = 1;
+                }
+                else
+                {
+                    gammaRate[i] = 0;
+                }
+            }
+            //Console.WriteLine($"Gamma Rate: {string.Join(",", gammaRate)}");
+            return gammaRate;
+        }
+
+        public static int[] GetEpsilonRate(int[] countOfOnes, int[] countOfZeros, int length)
+        {
+            var epsilonRate = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                if (countOfOnes[i] > countOfZeros[i])
+                {
+                    epsilonRate[i] = 0;
+                }
+                else
+                {
+                    epsilonRate[i] = 1;
+                }
+            }
+            //Console.WriteLine($"Epsilon Rate: {string.Join(",", epsilonRate)}");
+            return epsilonRate;
+        }
+
+        public static char[] GetOxyRate(string[] data, int length)
+        {
+            var oxyRate = new char[length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                var line = data[i];
+
+                Console.WriteLine(data[i]);
+
+                for (int j = 0; j < line.Length; j++)
+                {
+
+                }
+            }
+
+            return oxyRate;
+        }
+
+        public static char[] GetCo2Rate(string[] data, int length)
+        {
+            var co2Rate = new char[length];
+
+
+
+            return co2Rate;
+        }
+
     }
 }
