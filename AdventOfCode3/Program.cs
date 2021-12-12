e4822818e5a75bc9eb05ab49930750171a4a593e
ginte.karbauskaite1@gmail.com <ginte.karbauskaite1@gmail.com>
2021-12-12 03:07:00 +0200

    Day 3 pt.2 done

--- AdventOfCode3/Program.cs
+++ AdventOfCode3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,25 +9,25 @@ namespace AdventOfCode3
     {
         static void Main(string[] args)
         {
-            var data = File.ReadAllLines("Prefab.txt");
+            var data = File.ReadAllLines("Data.txt");
             var lineLength = data.First().Length;
 
-            var countOfOnes = GetCountOfOnes(data, lineLength);
-            var countOfZeros = GetCountOfZeros(data, lineLength);
+            //var countOfOnes = GetCountOfOnes(data, lineLength);
+            //var countOfZeros = GetCountOfZeros(data, lineLength);
 
-            var gammaRate = GetGammaRate(countOfOnes, countOfZeros, lineLength);
-            var epsilonRate = GetEpsilonRate(countOfOnes, countOfZeros, lineLength);
+            //var gammaRate = GetGammaRate(countOfOnes, countOfZeros, lineLength);
+            //var epsilonRate = GetEpsilonRate(countOfOnes, countOfZeros, lineLength);
 
             var oxyRating = GetOxyRate(data, lineLength);
             var co2Rating = GetCo2Rate(data, lineLength);
 
 
-            Console.WriteLine($"Count of Ones: {string.Join(",", countOfOnes)}");
-            Console.WriteLine($"Count of Zeros: {string.Join(",", countOfZeros)}\n");
-            Console.WriteLine($"Gamma Rate: {string.Join(",", gammaRate)}");
-            Console.WriteLine($"Epsilon Rate: {string.Join(",", epsilonRate)}\n");
-            //Console.WriteLine($"Oxygen rate: {string.Join(",", oxyRating)}");
-            //Console.WriteLine($"CO2 rate: {string.Join(",", co2Rating)}");
+            //Console.WriteLine($"Count of Ones: {string.Join(",", countOfOnes)}");
+            //Console.WriteLine($"Count of Zeros: {string.Join(",", countOfZeros)}\n");
+            //Console.WriteLine($"Gamma Rate: {string.Join(",", gammaRate)}");
+            //Console.WriteLine($"Epsilon Rate: {string.Join(",", epsilonRate)}\n");
+            Console.WriteLine($"Oxygen rate: {string.Join(",", oxyRating)}");
+            Console.WriteLine($"CO2 rate: {string.Join(",", co2Rating)}");
         }
 
         public static int[] GetCountOfOnes(string[] data, int length)
@@ -35,36 +36,33 @@ namespace AdventOfCode3
 
             foreach (var character in data)
             {
-                //Console.WriteLine($"Bits: {character}; Length: {character.Length};");
-
                 for (int i = 0; i < character.Length; i++)
                 {
                     if (character[i] == '1')
                         Ones[i]++;
                 }
             }
+            Console.WriteLine($"Count of Ones: {string.Join(",", Ones)}");
+
 
             return Ones;
         }
 
         public static int[] GetCountOfZeros(string[] data, int length)
         {
-            var countOfZeros = new int[length];
+            var Zeros = new int[length];
 
             foreach (var character in data)
             {
-                //Console.WriteLine($"Bits: {character}; Length: {character.Length};");
-
                 for (int i = 0; i < character.Length; i++)
                 {
-                    //Console.WriteLine($"Char: {character[i]};");
                     if (character[i] == '0')
-                        countOfZeros[i]++;
+                        Zeros[i]++;
                 }
-                //Console.WriteLine($"Count of Zeros: {countOfZeros[0]}");
             }
+            Console.WriteLine($"Count of Zeros: {string.Join(",", Zeros)}");
 
-            return countOfZeros;
+            return Zeros;
         }
 
         public static int[] GetGammaRate(int[] countOfOnes, int[] countOfZeros, int length)
@@ -103,33 +101,44 @@ namespace AdventOfCode3
             return epsilonRate;
         }
 
-        public static char[] GetOxyRate(string[] data, int length)
+        public static int[] GetOxyRate(string[] data, int length)
         {
-            var oxyRate = new char[length];
+            var subset = new List<string>(data);
 
-            for (int i = 0; i < data.Length; i++)
+            for (int i = 0; i < length; i++)
             {
-                var line = data[i];
+                if (subset.Count == 1) break;
 
-                Console.WriteLine(data[i]);
+                var zeros = subset.Count(x => x[i] == '0');
+                var ones = subset.Count(x => x[i] == '1');
+                var looikngFor = zeros == ones ? '1' : zeros > ones ? '0' : '1';
 
-                for (int j = 0; j < line.Length; j++)
-                {
-
-                }
+                subset = subset.Where(x => x[i] == looikngFor).ToList();
             }
 
-            return oxyRate;
+            var result = subset.First().Select(x => int.Parse(x.ToString())).ToArray();
+
+            return result;
         }
 
-        public static char[] GetCo2Rate(string[] data, int length)
+        public static int[] GetCo2Rate(IEnumerable<string> data, int length)
         {
-            var co2Rate = new char[length];
+            var subset = new List<string>(data);
 
+            for (int i = 0; i < length; i++)
+            {
+                if (subset.Count == 1) break;
 
+                var zeros = subset.Count(x => x[i] == '0');
+                var ones = subset.Count(x => x[i] == '1');
+                var looikngFor = zeros == ones ? '0' : zeros > ones ? '1' : '0';
 
-            return co2Rate;
+                subset = subset.Where(x => x[i] == looikngFor).ToList();
+            }
+
+            var result = subset.First().Select(x => int.Parse(x.ToString())).ToArray();
+
+            return result;
         }
-
     }
 }
