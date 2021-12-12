d5229ed797bdd96e7847056b9ddb16ef5a9f6b83
ginte.karbauskaite1@gmail.com <ginte.karbauskaite1@gmail.com>
2021-12-12 04:01:38 +0200

    Start of Day4

---
+++ AdventOfCode4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("Prefab.txt");
+
+            var drawnNumbers = data[0].Split(",", data[0].Length);
+            Console.WriteLine($"Drawn numbers are:\n{string.Join(" ", drawnNumbers)}\n\n");
+
+            for (int i = 2; i < data.Length; i++)
+            {
+                Console.WriteLine($"Nums: {data[i]}");
+                if (i%6 == 0)
+                {
+                    Console.WriteLine("\n       B  I  N  G  O \n");
+                    i++;
+                }
+            }
+        }
+    }
+}
